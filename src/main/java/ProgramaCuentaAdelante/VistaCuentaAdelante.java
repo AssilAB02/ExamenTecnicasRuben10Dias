8af9ec77e8dd5135f4e7a8bfbb2e70bb5f2118e1
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-10 23:31:55 +0200

    Implementacion VistaCuentaAdelante.java

--- src/main/java/ProgramaCuentaAdelante/VistaCuentaAdelante.java
+++ src/main/java/ProgramaCuentaAdelante/VistaCuentaAdelante.java
@@ -1,4 +1,36 @@
 package ProgramaCuentaAdelante;
 
+import javax.swing.*;
+
+/**
+ * Esta clase implementa la vista de la aplicación de contador adelante.
+ */
 public class VistaCuentaAdelante {
+    private JFrame frame;
+    private JLabel labelCuenta;
+
+    /**
+     * Constructor de la clase VistaCuentaAdelante.
+     * Crea y muestra la ventana de la aplicación.
+     */
+    public VistaCuentaAdelante() {
+        frame = new JFrame("Contador Adelante");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        labelCuenta = new JLabel();
+        labelCuenta.setBounds(20, 50, 250, 20);
+        frame.add(labelCuenta);
+
+        frame.setVisible(true);
+    }
+
+    /**
+     * Método para mostrar el número actual del contador en la vista.
+     * @param numero El número a mostrar en la vista.
+     */
+    public void mostrarCuenta(int numero) {
+        labelCuenta.setText(Integer.toString(numero));
+    }
 }
