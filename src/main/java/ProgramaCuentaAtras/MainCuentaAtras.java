d632f2704a4b3f1f0e297da3f2fc375a4cb094b5
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-10 23:20:52 +0200

    Implementacion MainCuentaAtras.java

--- src/main/java/ProgramaCuentaAtras/MainCuentaAtras.java
+++ src/main/java/ProgramaCuentaAtras/MainCuentaAtras.java
@@ -1,4 +1,12 @@
 package ProgramaCuentaAtras;
 
+
+
 public class MainCuentaAtras {
+    public static void main(String[] args) {
+        ModeloCuentaAtras modelo = new ModeloCuentaAtras(0);
+        VistaCuentaAtras vista = new VistaCuentaAtras();
+        ControladorCuentaAtras controlador = new ControladorCuentaAtras(modelo, vista);
+        controlador.iniciarConteo();
+    }
 }
