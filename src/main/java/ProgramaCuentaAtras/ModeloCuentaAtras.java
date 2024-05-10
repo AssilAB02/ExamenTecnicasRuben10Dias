2e3dd5e48f34071d0b58022246ec917eb23827dd
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-10 23:11:17 +0200

    Implementacion funciones ModeloCuentaAtras.java

--- src/main/java/ProgramaCuentaAtras/ModeloCuentaAtras.java
+++ src/main/java/ProgramaCuentaAtras/ModeloCuentaAtras.java
@@ -2,4 +2,18 @@ package ProgramaCuentaAtras;
 
 public class ModeloCuentaAtras {
 
+    private int contador;
+
+    public ModeloCuentaAtras(int contadorInicial) {
+        this.contador = contadorInicial;
+    }
+
+    public int obtenerContador() {
+        return contador;
+    }
+
+    public void decrementarContador() {
+        contador--;
+    }
 }
+
