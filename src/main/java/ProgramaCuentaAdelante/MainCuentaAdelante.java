6ab1d82ced64931de537af49bfd54be8dbeeb520
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-10 23:33:22 +0200

    MainCuentaAdelante.java

--- src/main/java/ProgramaCuentaAdelante/MainCuentaAdelante.java
+++ src/main/java/ProgramaCuentaAdelante/MainCuentaAdelante.java
@@ -1,4 +1,17 @@
 package ProgramaCuentaAdelante;
 
+/**
+ * Esta clase implementa la aplicación de contador adelante.
+ */
 public class MainCuentaAdelante {
+    /**
+     * Método principal para iniciar la aplicación.
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este programa).
+     */
+    public static void main(String[] args) {
+        ModeloCuentaAdelante modelo = new ModeloCuentaAdelante(0);
+        VistaCuentaAdelante vista = new VistaCuentaAdelante();
+        ControladorCuentaAdelante controlador = new ControladorCuentaAdelante(modelo, vista);
+        controlador.iniciarConteo();
+    }
 }
