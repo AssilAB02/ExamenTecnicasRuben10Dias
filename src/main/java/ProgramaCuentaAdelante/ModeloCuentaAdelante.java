9c449b1bf1f868d062fd7ca9c3a6ce1d64760fdd
AssilAB02 <150059237+AssilAB02@users.noreply.github.com>
2024-05-10 23:29:43 +0200

    Implementacion ModeloCuentaAdelante.java

--- src/main/java/ProgramaCuentaAdelante/ModeloCuentaAdelante.java
+++ src/main/java/ProgramaCuentaAdelante/ModeloCuentaAdelante.java
@@ -1,4 +1,31 @@
 package ProgramaCuentaAdelante;
 
+/**
+ * Esta clase implementa el modelo de contador adelante.
+ */
 public class ModeloCuentaAdelante {
+    private int contador;
+
+    /**
+     * Constructor de la clase ContadorAdelante.
+     * @param contadorInicial El número inicial del contador.
+     */
+    public ModeloCuentaAdelante(int contadorInicial) {
+        this.contador = contadorInicial;
+    }
+
+    /**
+     * Método para obtener el valor actual del contador.
+     * @return El valor actual del contador.
+     */
+    public int obtenerContador() {
+        return contador;
+    }
+
+    /**
+     * Método para incrementar el contador en una unidad.
+     */
+    public void incrementarContador() {
+        contador++;
+    }
 }
